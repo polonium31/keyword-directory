124b24f31ea491aab01b87b4f15ea13444cf6b74
tanay388 <tanay.deo388@gmail.com>
2023-09-17 12:42:20 +0530

    Fix: Incorrect user initialization in hook

--- src/context.jsx
+++ src/context.jsx
@@ -9,7 +9,7 @@ import { auth } from "./firebase";
 const userAuthContext = createContext();
 
 export function UserAuthContextProvider({ children }) {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   function logIn(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
